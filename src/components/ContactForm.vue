edc93be3a85f9a99ae93b5e2eabfaa7605759551
Marie-Claire <mcbalabanian@thisismess.com>
2024-03-12 10:13:29 -0700

    contacct form test

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,145 +1,152 @@
 <template>
-      <!-- 'contact' section design and elements -->
-  <section class="contact" id="contact">
-    <h2 class="heading">Contact <span>Me!</span></h2>
-    <form ref="formRef" @submit.prevent="sendEmail">
-      <div class="input-box">
-        <div class="input-field">
-          <input type="text" placeholder="Full Name" name="name" required>
-        </div>
-        <div class="input-field">
-          <input type="email" placeholder="Email Address" name="email" required>
-        </div>
-      </div>
-      <div class="input-box">
-        <div class="input-field">
-          <input type="tel" placeholder="Mobile Number" name="phone" required>
-        </div>
-        <div class="input-field">
-          <input type="text" placeholder="Email Subject" name="subject" required>
-        </div>
-      </div>
-      <div class="textarea-field">
-        <textarea id="message" cols="30" rows="10" placeholder="Your Message" name="message" required></textarea>
-      </div>
-      <div class="btn-box btns">
-        <button type="submit" class="btn">Submit</button>
-      </div>
-    </form>
-  </section>
+  <!-- 'contact' section design and elements -->
+<section class="contact" id="contact">
+<h2 class="heading">Contact <span>Me!</span></h2>
+<form ref="formRef" @submit.prevent="sendEmail">
+  <div class="input-box">
+    <div class="input-field">
+      <input v-model="name"  type="text" placeholder="Full Name" name="name" required>
+    </div>
+    <div class="input-field">
+      <input v-model="email" type="email" placeholder="Email Address" name="email" required>
+    </div>
+  </div>
+  <div class="input-box">
+    <div class="input-field">
+      <input v-model="phone"  type="tel" placeholder="Mobile Number" name="phone" required>
+    </div>
+    <div class="input-field">
+      <input v-model="subject" type="text" placeholder="Email Subject" name="subject" required>
+    </div>
+  </div>
+  <div class="textarea-field">
+    <textarea v-model="message" id="message" cols="30" rows="10" placeholder="Your Message" name="message" required></textarea>
+  </div>
+  <div class="btn-box btns">
+    <button type="submit" class="btn">Submit</button>
+  </div>
+</form>
+</section>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import emailjs from '@emailjs/browser'
 
-const formRef = ref(null)
+const message = ref('')
+const email = ref('')
+const name = ref('')
+const subject = ref('')
+const phone = ref('')
 
 const sendEmail = () => {
-      emailjs
-        .sendForm(import.meta.env.VITE_EJS_ID, import.meta.env.VITE_EJS_TEMPLATE, formRef.value, {
-          publicKey: import.meta.env.VITE_EJS_KEY,
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          },
-        );
-    }
+const data = {
+    From: "hello@mcbalabanian.io",
+    To: "hello@mcbalabanian.io",
+    Subject: subject.value,
+    HtmlBody: `
+        <h2>${name.val}</h2>
+        <h3>${email.value}</h3>
+        <p>${phone.value}</p>
+        <p>${message.value}</p>
+        
+    `,
+};
+fetch('/api/contact', {
+    method: "POST",
+    body: JSON.stringify(data)
+})
+}
 </script>
 
 <style scoped>
 .contact {
-  min-height: auto;
-  padding-bottom: 7rem;
-  background: white;
+min-height: auto;
+padding-bottom: 7rem;
+background: white;
 }
 
 .contact h2 {
-  text-align: center;
+text-align: center;
 }
 
 .contact form {
-  max-width: 70rem;
-  margin: 0 auto;
-  text-align: center;
+max-width: 70rem;
+margin: 0 auto;
+text-align: center;
 }
 
 .contact form .input-box {
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
+display: flex;
+justify-content: space-between;
+flex-wrap: wrap;
 }
 
 .contact form .input-box .input-field {
-  width: 49%;
-  margin: 0.8rem 0;
+width: 49%;
+margin: 0.8rem 0;
 }
 
 .contact form .input-box .input-field input,
 .contact form .textarea-field textarea {
-  width: 100%;
-  padding: 1.5rem;
-  font-size: 1.6rem;
-  color: var(--text-color);
-  background: transparent;
-  border-radius: 0.6rem;
-  border: 0.2rem solid var(--main-color);
-  transition: border-color 0.3s ease;
+width: 100%;
+padding: 1.5rem;
+font-size: 1.6rem;
+color: var(--text-color);
+background: transparent;
+border-radius: 0.6rem;
+border: 0.2rem solid var(--main-color);
+transition: border-color 0.3s ease;
 }
 
 .contact form .input-box .input-field input::placeholder,
 .contact form .textarea-field textarea::placeholder {
-  color: var(--text-color);
+color: var(--text-color);
 }
 
 .contact form .input-box .input-field input:focus,
 .contact form .input-box .input-field input:valid,
 .contact form .textarea-field textarea:focus,
 .contact form .textarea-field textarea:valid {
-  outline: none;
-  border-color: var(--second-bg-color);
+outline: none;
+border-color: var(--second-bg-color);
 }
 
 .contact form .input-box .input-field input:focus,
 .contact form .textarea-field textarea:focus {
-  border-color: var(--main-color);
+border-color: var(--main-color);
 }
 
 .contact form .input-box .input-field input:focus::placeholder,
 .contact form .textarea-field textarea:focus::placeholder {
-  color: transparent;
+color: transparent;
 }
 
 .contact form .focus {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 100%;
-  background: var(--second-bg-color);
-  border-radius: 0.6rem;
-  z-index: -1;
-  transition: width 0.3s ease;
+position: absolute;
+top: 0;
+left: 0;
+width: 0;
+height: 100%;
+background: var(--second-bg-color);
+border-radius: 0.6rem;
+z-index: -1;
+transition: width 0.3s ease;
 }
 
 .contact form .textarea-field {
-  margin: 0.8rem 0 2.7rem;
+margin: 0.8rem 0 2.7rem;
 }
 
 .contact form .textarea-field textarea {
-  resize: none;
+resize: none;
 }
 
 .contact form .btn-box.btns .btn {
-  cursor: pointer;
+cursor: pointer;
 }
 @media (max-width: 462px) {
-  .contact form .input-box .input-field {
-    width: 100%;
-  }
+.contact form .input-box .input-field {
+width: 100%;
+}
 }
 </style>
