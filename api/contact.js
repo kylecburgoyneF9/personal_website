218c75da3d69e3d33fce7f300f58f4ee754c99cf
Marie-Claire <mcbalabanian@thisismess.com>
2024-03-12 12:24:22 -0700

    verc

--- api/contact.js
+++ api/contact.js
@@ -1,16 +1,15 @@
 import postmark from 'postmark'
 
 export default async function(req, res) {
-    const client = new postmark.ServerClient(process.env.POSTMARK);
-    console.log(req.body)
-    try {
-       await client.sendEmail(req.body).then(() => {
-        res.status(200).send('Message sent successfully.')
+  const client = new postmark.ServerClient(process.env.POSTMARK);
+  try {
+      await client.sendEmail(req.body).then(() => {
+      res.status(200).send('Message sent successfully.')
 
-       })
-    } catch (error) {
-      console.log('ERROR', error)
-      res.status(400).send('Message not sent.')
-    }
+      })
+  } catch (error) {
+    console.log('ERROR', error)
+    res.status(400).send('Message not sent.')
   }
+}
 
