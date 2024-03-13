2bfd558896734de25d1b74a3f69770488187b659
Marie-Claire <mcbalabanian@thisismess.com>
2024-03-13 00:36:06 -0700

    aoi thing

--- api/contact.js
+++ api/contact.js
@@ -4,16 +4,35 @@ export default async function (req, res) {
   const data = JSON.parse(req.body)
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-  const msg = {
-    to: `${process.env.SENDGRID_TO}`, //  your recipient
-    from: `${process.env.SENDGRID_FROM}`, //  your verified sender of sendgrid account
-    subject: `${data.subject}`,
-    html: `
-      <h2>${data.name}</h2>
-      <h3>${data.email}</h3>
-      <h3>${data.phone}</h3>
-      <p>${data.message}</p>`
+  let msg;
+
+  if (req?.body) {
+    msg = {
+      to: `${process.env.SENDGRID_TO}`, //  your recipient
+      from: `${process.env.SENDGRID_FROM}`, //  your verified sender of sendgrid account
+      subject: `${data.subject}`,
+      html: `
+        <h2>${data.name}</h2>
+        <h3>${data.email}</h3>
+        <h3>${data.phone}</h3>
+        <p>${data.message}</p>`
+    }
   }
+
+  else {
+    msg = {
+      to: `${process.env.SENDGRID_TO}`, //  your recipient
+      from: `${process.env.SENDGRID_FROM}`, //  your verified sender of sendgrid account
+      subject: `helloo`,
+      html: `
+        <h2>helloo</h2>
+        <h3>helloo</h3>
+        <h3>helloo</h3>
+        <p>helloo</p>`
+    }
+  }
+
+   
 
   try {
     await sgMail
